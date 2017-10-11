114f6c19223dce7b47988213e2f215ef508c6d86
GaoChengQuan <529390053@qq.com>
2017-10-11 16:18:25 +0800

    添加商品到购物车

--- src/com/situ/mall/controller/front/CartController.java
+++ src/com/situ/mall/controller/front/CartController.java
@@ -1,17 +1,75 @@
 package com.situ.mall.controller.front;
 
+import java.io.StringWriter;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.situ.mall.pojo.Product;
+import com.situ.mall.service.IProductService;
+import com.situ.mall.vo.BuyCartVO;
+import com.situ.mall.vo.CartItemVO;
 
 @Controller
 @RequestMapping("/cart")
 public class CartController {
+	@Autowired
+	private IProductService productService;
 
 	@RequestMapping("/addCart")
-	public String addCart(Integer productId, Integer amount) {
+	public String addCart(Integer productId, Integer amount, Model model, HttpServletRequest request) {
 		System.out.println("productId:" + productId);
 		System.out.println("amount:" + amount);
 		
+		//springmvc
+		ObjectMapper objectMapper = new ObjectMapper();
+		
+		BuyCartVO buyCartVO = null;
+		Cookie[] cookies = request.getCookies();
+		//1.如果cookie有这个购物车对象，那就从cookie里面取出这个购物车对象
+		if (null != cookies && cookies.length > 0) {
+			for (Cookie cookie : cookies) {
+				if ("buy_cart_cookie".equals(cookie.getName())) {
+					//之前已经有购物车
+					
+				}
+			}
+		}
+		//2.如果cookie没有这个购物车对象,才需要new BuyCartVO
+		if (buyCartVO == null) {
+			buyCartVO = new BuyCartVO();
+		}
+		
+		//把购物项放到购物车里面
+		if (null != productId) {
+			Product product = productService.findById(productId);
+			CartItemVO cartItemVO = new CartItemVO();
+			cartItemVO.setProduct(product);
+			cartItemVO.setAmount(amount);
+			
+			buyCartVO.addItem(cartItemVO);
+			buyCartVO.setProductId(productId);
+			
+			//把购物车对象BuyCartVO以json形式写到cookie里面
+			StringWriter stringWriter = new StringWriter();
+			
+			
+		}
+		
+		//3.将购物车放到cookie
+		
+		
+		
+		
+		
+		
+		model.addAttribute("buyCartVO", buyCartVO);
 		return "cart";
 	}
 }
--- src/com/situ/mall/vo/BuyCartVO.java
+++ src/com/situ/mall/vo/BuyCartVO.java
@@ -3,6 +3,8 @@ package com.situ.mall.vo;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.junit.validator.PublicClassValidator;
+
 /**
  * 购物车
  *
@@ -13,6 +15,30 @@ public class BuyCartVO {
 
 	// 继续购物，最后一次产品的id
 	private Integer productId;
+	
+	public void addItem(CartItemVO cartItemVO) {
+		boolean isExist = false;
+		//1.这件商品原来在购物车里面有，我们只需要更新购买数量
+		for (CartItemVO item : items) {
+			if (item.getProduct().getId() == cartItemVO.getProduct().getId()) {
+				isExist = true;
+				int amount = item.getAmount() + cartItemVO.getAmount();
+				//购买这件商品的总数量应该<=product.stock
+				if (amount <= item.getProduct().getStock()) {
+					item.setAmount(amount);
+				} else {
+					//超出购买限制，最大只能购买这件商品的最大库存
+					item.setAmount(item.getProduct().getStock());
+				}
+				break;
+			}
+		}
+		//2.这件商品原来没有加入购物车,加入购物车
+		if (isExist == false) {
+			items.add(cartItemVO);
+		}
+		
+	}
 
 	public List<CartItemVO> getItems() {
 		return items;
@@ -34,5 +60,7 @@ public class BuyCartVO {
 	public String toString() {
 		return "BuyCartVO [items=" + items + ", productId=" + productId + "]";
 	}
+
+	
 
 }
